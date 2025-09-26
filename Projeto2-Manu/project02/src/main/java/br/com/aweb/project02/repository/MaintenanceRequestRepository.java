83fe3fe54a99b290cc25816ecfd36c02a170e04e
Matheus Reis <130017997+Theus012@users.noreply.github.com>
2025-09-26 14:22:27 -0300

    Delete Projeto2-Manu directory

--- Projeto2-Manu/project02/src/main/java/br/com/aweb/project02/repository/MaintenanceRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.aweb.project02.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.aweb.project02.model.MaintenanceRequest;
-
-public interface MaintenanceRequestRepository extends JpaRepository<MaintenanceRequest, Long> {
-    
-}
